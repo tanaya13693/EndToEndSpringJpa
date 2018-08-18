a7de85d98e6c4c5b0d3fe0b7b0d2f38617df72c5
Vadgave <tvadgave@deloitte.com>
2018-08-18 16:51:35 -0400

    Added Integration layer

---
+++ locationweb/src/main/java/com/tanaya/location/controllers/LocationRestController.java
@@ -0,0 +1,56 @@
+package com.tanaya.location.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tanaya.location.entities.Location;
+import com.tanaya.location.repos.LocationRepository;
+
+@RestController
+@RequestMapping("/locations")
+public class LocationRestController {
+	
+	@Autowired
+	LocationRepository locationRepository;
+	
+	@GetMapping
+	public List<Location> getLocations(){
+		
+		return locationRepository.findAll();
+	}
+
+	@PostMapping
+	public Location createLocation(@RequestBody Location location){
+		
+		return locationRepository.save(location);
+	}
+	
+	@PutMapping
+	public Location updateLocation(@RequestBody Location location){
+		
+		return locationRepository.save(location);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteLocation(@PathVariable("id") int id){
+		
+		locationRepository.deleteById(id);
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Location> getLocation(@PathVariable("id") int id){
+		
+		return locationRepository.findById(id);
+	}
+	
+}
